b4f715ac0dc2df3c662c2b1c76fe30b5a81ea068
Hololens Dev Pc <bitter.daniel@gmail.com>
2020-06-28 10:34:39 -0500

    Put this script on the camera to click and rotate

---
+++ Assets/AzureKinectDK/Examples/Scripts/CameraOrbit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+    public Transform parent;
+    public float travelSpeed = 1.0f;
+    private void Update()
+    {
+        if (parent.childCount > 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Debug.Log(parent.GetChild(0).name);
+                transform.LookAt(parent.GetChild(0));
+                Vector2 rotateDir = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+                transform.Translate(travelSpeed * rotateDir);
+            }
+        }
+    }
+}
